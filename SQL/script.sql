93125e3ed2d5cd9b52f9167b4ff67929b2aa4037
olyavick <olyavickulowa@yandex.ru>
2017-05-22 17:58:01 +0300

    create cards and text

--- SQL/script.sql
+++ SQL/script.sql
@@ -1,5 +1,7 @@
 CREATE TABLE clients (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, email VARCHAR(40) NOT NULL,
-	password VARCHAR(40) NOT NULL, name VARCHAR(20) NOT NULL, lastname VARCHAR(30) NOT NULL, 
-	birthdate DATE NOT NULL, sex VARCHAR(1) NOT NULL, phone VARCHAR(12) NOT NULL);
+	pass VARCHAR(40) NOT NULL, first_name VARCHAR(20) NOT NULL, last_name VARCHAR(30) NOT NULL, 
+	birthdate DATE NOT NULL, sex VARCHAR(1) NOT NULL, phone VARCHAR(12) NOT NULL, UNIQUE (email));
 
-CREATE CONSTRAINT clients UNIQUE (email);
+INSERT INTO clients (email, pass, first_name, last_name, birthdate, sex, phone) 
+	VALUES ('qwerty@yandex.ru', 'fhbvjbdsbsbfwefjwefbkfbs','Коля', 'Другой', 
+	STR_TO_DATE('1995-01-01','%Y-%m-%d'), 'М', '+79162917261');
